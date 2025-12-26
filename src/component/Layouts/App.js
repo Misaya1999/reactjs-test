e2cad00b3b834b66b4e6833b86ae9e8283f30f29
Misaya <misaya9991@gmail.com>
2025-12-26 21:29:32 +0700

    update login UI

---
+++ src/component/Layouts/App.js
@@ -0,0 +1,15 @@
+import Footer from "./Footer";
+import Header from "./Header";
+import LeftSidebar from "./LeftSidebar";
+
+function App() {
+  return (
+    <>
+        <Header/>
+        <LeftSidebar/>
+        <Footer/>
+    </>
+  );
+}
+
+export default App;
