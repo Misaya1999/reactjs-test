78dfe00291b3694dac91b3ab1f51447c6c7f6d34
Misaya <misaya9991@gmail.com>
2026-01-19 11:09:40 +0700

    add react router

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,7 +1,32 @@
 function Login() {
   return (
     <div className="App">
-        Login
+      <div className="row" style={{marginBottom: '20px', display: 'flex', justifyContent: 'center'}}>
+        <div className="col-sm-4">
+          <div className="login-form">
+            <h2>Login!</h2>
+            <form method="POST" action="{{ route('users.login.form') }}">
+              @csrf
+              <input type="email" name="email" placeholder="Email Address" />
+              <input type="password" name="password" placeholder="Password" />
+              <span>
+                <input type="checkbox" className="checkbox" name="remember_me" /> 
+                Remember me
+              </span>
+              <div className="button-group" style={{display: 'flex', gap: '10px'}}>
+                <button type="submit" className="btn btn-default">Login</button>
+                <button type="button" className="btn btn-default" onclick="window.location='{{ route('users.register.form') }}'">Register</button>
+                <button type="button" className="btn btn-default" onclick="window.location='{{ route('users.forgotPassword.form') }}'">Forgot Password ?</button>
+              </div>
+              @if ($errors-&gt;has('login'))
+              <div className="alert alert-danger">
+                {'{'}{'{'} $errors-&gt;first('login') {'}'}{'}'}
+              </div>
+              @endif
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
